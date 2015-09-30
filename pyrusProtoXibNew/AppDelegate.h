23336f00f817052833da7ab7a8eee8c47e44213b
aeternas <aeternass@gmail.com>
2015-09-30 19:19:14 +0300

    Initial Commit

---
+++ pyrusProtoXibNew/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  pyrusProtoXibNew
+//
+//  Created by AIR on 30.09.15.
+//  Copyright © 2015 Ivan Golikov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ pyrusProtoXibNew/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  pyrusProtoXibNew
+//
+//  Created by AIR on 30.09.15.
+//  Copyright © 2015 Ivan Golikov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
